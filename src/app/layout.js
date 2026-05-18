5de1346e36b63a8574f6329100a26c0c5328c1d1
TIM-Coder07 <sajonwdd72566@gmail.com>
2026-05-18 06:26:10 +0600

    The footer part is complite

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <NavBar></NavBar>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
